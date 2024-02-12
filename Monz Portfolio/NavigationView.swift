36ca7f3da4a028a66cf949a95a2baa7e5c0b00d6
Harry Hocker <69062179+SlimyMonz@users.noreply.github.com>
2024-02-12 09:48:15 -0500

    Changed scope to iOS only.

---
+++ Monz Portfolio/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  Monz Portfolio
+//
+//  Created by Harry Hocker on 2/12/24.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
